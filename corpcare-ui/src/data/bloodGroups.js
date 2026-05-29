68b385a5d4d6cf780c470e1ffc03b9838ddc601d
Partha-2 <mparthasarathy0000@gmail.com>
2026-05-29 22:07:10 +0530

    Remove leftover CorpCare UI code

--- corpcare-ui/src/data/bloodGroups.js
+++
@@ -1,5 +0,0 @@
-const BG_LABELS = {
-  A_POSITIVE: 'A+', A_NEGATIVE: 'A-', B_POSITIVE: 'B+', B_NEGATIVE: 'B-',
-  O_POSITIVE: 'O+', O_NEGATIVE: 'O-', AB_POSITIVE: 'AB+', AB_NEGATIVE: 'AB-'
-}
-export default BG_LABELS
